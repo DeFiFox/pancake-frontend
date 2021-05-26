3daf155189ad07f3734b0f81f8828e55f75eda48
DeFiFox <84774788+DeFiFox@users.noreply.github.com>
2021-05-26 10:02:43 +0700

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -3,11 +3,22 @@ const tokens = {
     symbol: 'BNB',
     projectLink: 'https://www.binance.com/',
   },
+  
   cake: {
     symbol: 'CAKE',
     address: {
       56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
       97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
+    },
+    decimals: 18,
+    projectLink: 'https://pancakeswap.finance/',
+  },
+  
+   wbnb: {
+    symbol: 'wBNB',
+    address: {
+      56: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+      97: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
     },
     decimals: 18,
     projectLink: 'https://pancakeswap.finance/',
