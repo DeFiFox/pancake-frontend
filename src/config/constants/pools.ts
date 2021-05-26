269359f372ee4a3edfee4095c72db4863e8855a5
DeFiFox <84774788+DeFiFox@users.noreply.github.com>
2021-05-27 00:12:47 +0700

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -16,21 +16,7 @@ const pools: PoolConfig[] = [
     sortOrder: 1,
     isFinished: false,
   },
-  
-  {
-    sousId: 20,
-    stakingToken: tokens.cake,
-    earningToken: tokens.tkf,
-    contractAddress: {
-      97: '',
-      56: '0x5db85cafD3AABA39F20e502d28b93be0bd2b14F0',
-    },
-    poolCategory: PoolCategory.CORE,
-    harvest: true,
-    tokenPerBlock: '100',
-    sortOrder: 20,
-  },
- 
+
 ]
 
 export default pools
