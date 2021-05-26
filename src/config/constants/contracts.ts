f2569aa13b50377ded0a346bf9d5800dbdc0963c
DeFiFox <84774788+DeFiFox@users.noreply.github.com>
2021-05-26 23:27:25 +0700

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,7 +1,7 @@
 export default {
   masterChef: {
-    97: '0x0c03568f008000f44901d5dc2d492b415036c928',
-    56: '0x191C8870e9886c73E7cf23eB72815B7EE39b574a',
+    97: '0x45911bA1f8C31A1f53bEB8f2C2ABC5AB49fF4307',
+    56: '0x45911bA1f8C31A1f53bEB8f2C2ABC5AB49fF4307',
   },
   sousChef: {
     97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
