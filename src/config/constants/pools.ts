9f6dc909d81a4093896e5253068d662493bd88e7
DeFiFox <84774788+DeFiFox@users.noreply.github.com>
2021-05-27 08:20:46 +0700

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -12,7 +12,7 @@ const pools: PoolConfig[] = [
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '1000',
+    tokenPerBlock: '100',
     sortOrder: 1,
     isFinished: false,
   },
