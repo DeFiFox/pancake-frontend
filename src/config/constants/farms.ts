54980a507984ff0a88529ad664f0c3850853e534
DeFiFox <84774788+DeFiFox@users.noreply.github.com>
2021-05-28 21:54:29 +0700

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -16,7 +16,15 @@ const farms: FarmConfig[] = [
     token: tokens.cake,
     quoteToken: tokens.wbnb,
   },
-  
+    pid: 252,
+    lpSymbol: 'BUSD-BNB LP',
+    lpAddresses: {
+      97: '0x27a522c1762f166c3bb8898cb1f592552da71f18',
+      56: '0x20c33FB85535c4308CeA5409aE8A424BABC74168',
+    },
+    token: tokens.busd,
+    quoteToken: tokens.wbnb,
+  },
   
  
   
