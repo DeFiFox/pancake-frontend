4b683233f9565a7861276421ff8cfb8557a387bb
DeFiFox <84774788+DeFiFox@users.noreply.github.com>
2021-05-28 23:11:06 +0700

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -7,16 +7,35 @@ const farms: FarmConfig[] = [
    */
   
   {
+    pid: 0,
+    lpSymbol: 'CAKE',
+    lpAddresses: {
+      97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
+      56: '0xd8ee050845db3eb1695d748f807dfc93ea08ef1c',
+    },
+    token: tokens.syrup,
+    quoteToken: tokens.wbnb,
+  },
+ {
     pid: 251,
     lpSymbol: 'CAKE-BNB LP',
     lpAddresses: {
-      97: '0x27a522c1762f166c3bb8898cb1f592552da71f18',
-      56: '0x20c33FB85535c4308CeA5409aE8A424BABC74168',
+      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
+      56: '0x20c33fb85535c4308cea5409ae8a424babc74168',
     },
     token: tokens.cake,
     quoteToken: tokens.wbnb,
   },
- 
+  {
+    pid: 252,
+    lpSymbol: 'BUSD-BNB LP',
+    lpAddresses: {
+      97: '',
+      56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
+    },
+    token: tokens.busd,
+    quoteToken: tokens.wbnb,
+  },
   
  
   
