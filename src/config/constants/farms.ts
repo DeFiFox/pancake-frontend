3d498ee68ce0b6e60b108a7f9508ab717af29a28
DeFiFox <84774788+DeFiFox@users.noreply.github.com>
2021-05-28 21:49:01 +0700

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -11,21 +11,12 @@ const farms: FarmConfig[] = [
     lpSymbol: 'CAKE-BNB LP',
     lpAddresses: {
       97: '0x27a522c1762f166c3bb8898cb1f592552da71f18',
-      56: '0x27a522c1762f166c3bb8898cb1f592552da71f18',
+      56: '0x20c33FB85535c4308CeA5409aE8A424BABC74168',
     },
     token: tokens.cake,
     quoteToken: tokens.wbnb,
   },
-   {
-    pid: 252,
-    lpSymbol: 'BUSD-BNB LP',
-    lpAddresses: {
-      97: '0x27a522c1762f166c3bb8898cb1f592552da71f18',
-      56: '0x27a522c1762f166c3bb8898cb1f592552da71f18',
-    },
-    token: tokens.cake,
-    quoteToken: tokens.wbnb,
-  },
+  
   
  
   
