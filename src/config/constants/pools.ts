7b221799d998d1bd8238f3a3b9082f26a25fda69
DeFiFox <84774788+DeFiFox@users.noreply.github.com>
2021-05-29 12:30:35 +0700

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -4,15 +4,15 @@ import { PoolConfig, PoolCategory } from './types'
 const pools: PoolConfig[] = [
   {
     sousId: 0,
-    stakingToken: tokens.cake,
-    earningToken: tokens.cake,
+    stakingToken: tokens.tika,
+    earningToken: tokens.tika,
     contractAddress: {
       97: '',
       56: '0xce86b5e0102c089210199D8EcB80fdD26520C738',
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '50',
+    tokenPerBlock: '20',
     sortOrder: 1,
     isFinished: false,
   },
