3b650b67cd31f458b8cf0985ea1ec28104fef2a3
DeFiFox <84774788+DeFiFox@users.noreply.github.com>
2021-05-29 11:21:04 +0700

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -91,9 +91,9 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" scale="xl" mb="24px" color="secondary">
-          {t('PancakeSwap')}
+          {t('TikaSwap')}
         </Heading>
-        <Text>{t('The #1 AMM and yield farm on Binance Smart Chain.')}</Text>
+        <Text>{t('The #1 AMM and High APR yield farm on Binance Smart Chain.')}</Text>
       </Hero>
       <div>
         <Cards>
